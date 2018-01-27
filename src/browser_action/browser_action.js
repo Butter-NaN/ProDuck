5935082ec56ac8b2422f1f348b5d8ff31d4dc9b3
zioul123 <zioul.rsmod123@gmail.com>
2018-01-28 02:31:40 +0800

    Add the tabmap functionality and pinning functions
    Add the function to add tabs to work and rest categories,
    as well as the function to toggle all pins in the tabMap

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -2,6 +2,10 @@
 // DEFINITIONS //
 /////////////////
 
+/**
+ * State functions
+ */
+
 // generates onclick function for
 //   - button#toggleStateButton
 //   - button#toggleTrackButton
@@ -29,6 +33,82 @@ function initHtmlValues(key, id_attr) {
         }
     );
 }
+
+/**
+ * Tab map handling
+ */
+
+// Adds the tab into the tabMap with stage stage,
+// stored as [tabId, cat(egory)]
+// Will replace if the tab already has a cat.
+function addTabCat(tab, stage) {
+    // Access the tabMap
+    var tabMap;
+    chrome.storage.local.get('tabMap',
+                    function(item) {
+                        console.log(item.tabMap);
+                        tabMap = item.tabMap;
+                    });
+    // Use the tab.id as a key to store the stage
+    tabMap[tab.id] = [];
+    tabMap[tab.id][0] = stage;
+}
+
+// Change/add the cat of the current tab to work
+function addCurrentToWork() {
+    chrome.tabs.query( { 'active': true,
+                         'currentWindow': true },
+                       function(tabs) {
+                           console.log(tabs[0].id);
+                           addTabCat(tabs[0], "work");
+                       });
+}
+
+// Change/add the cat of the current tab to rest
+function addCurrentToRest() {
+    chrome.tabs.query( { 'active': true,
+                         'currentWindow': true },
+                       function(tabs) {
+                           console.log(tabs[0].id);
+                           addTabCat(tabs[0], "rest");
+                       });
+}
+
+/**
+ * Tab Pin handling
+ */
+
+// Go through the tabMap and toggle all pins
+function toggleTabMapPins() {
+    // Retrieve the tabMap
+    var tabMap;
+    chrome.storage.local.get('tabMap',
+                    function(item) {
+                        console.log(item.tabMap);
+                        tabMap = item.tabMap;
+                    });
+    // retrieve current state
+    var state;
+    chrome.storage.local.get('state',
+                    function(item) {
+                        console.log(item.state);
+                        state = item.state;
+                    });
+
+    for (tab in tabMap) {
+        // Unpin all in the current state
+        if (tab[0] === state) {
+            chrome.tabs.update(tab, 
+                               {'pinned': false },
+                               function() {});
+        } else {
+            chrome.tabs.update(tab, 
+                               {'pinned': true },
+                               function() {});
+        }
+    };
+}
+
 
 ///////////////
 // EXECUTION //
@@ -63,6 +143,7 @@ $(document).ready(
             toggleCallbackFactory("track", true, false)
         );
         
+
         // initialise state of spans
         initHtmlValues('state', '#browserActionState');
         initHtmlValues('track', '#browserActionTrack');

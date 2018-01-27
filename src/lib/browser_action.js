bf9c00a115614a886f6a416be922854923427517
remo5000 <vigneshshankar@gmail.com>
2018-01-27 19:21:35 +0800

    migrate browser-action.js functionality to lib/browser-action.js

--- src/lib/browser_action.js
+++ src/lib/browser_action.js
@@ -5,9 +5,17 @@ function get_state() {
 }
 
 function toggle_state(last_state) {
+    console.log("toggling state from " + last_state);
     return last_state == STATE_REST ? STATE_WORK : STATE_REST;
 }
 
 function toggle_state_callback() {
     toggle_state(get_state());
+    console.log("state toggled. State is now " + get_state());
 }
+
+
+// action for button#toggleStateButton
+$(document).ready(function() {
+    $("#toggleStateButton").click(toggle_state_callback);
+});

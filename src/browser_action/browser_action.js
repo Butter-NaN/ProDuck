b12e2bb5e582589f8b71272f2c83d8dc230c55ab
ChengYao Chik <chikben@gmail.com>
2018-01-27 21:29:49 +0800

    Add start and stop timer buttons to browser action

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -13,8 +13,21 @@ function toggle_state_callback() {
     );
 }
 
+function start_timer(){
+    var timeNow = $.now();
+    console.log("Starting timer - time now is " + timeNow + "ms.");
+    chrome.runtime.sendMessage({"message": "startTimer", "time": timeNow})
+}
+
+function stop_timer(){
+    console.log("Stopping timer");
+    chrome.runtime.sendMessage({"message": "stopTimer"});
+}
+
 // action for button#toggleStateButton
 $(document).ready(function() {
     console.log('$(document).ready call');
     $("#toggleStateButton").click(toggle_state_callback);
+    $("#startTimerButton").click(start_timer);
+    $("#stopTimerButton").click(stop_timer);
 });
